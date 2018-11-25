a87ec66c537867ff1454fe96c544c7bdda3af320
saritdi <45334085+saritdi@users.noreply.github.com>
2018-11-25 19:37:44 +0200

    AS

--- src/Main_Menu.py
+++ src/Main_Menu.py
@@ -1,14 +1,15 @@
 import pro1
-
+import course_resize
 def Main_Menu():
     print('')
     choice = int(input(''))
     if choice==1:
-        InputCourse()
+        pro1.InputCourse()
         print('Manager')
     elif choice==2:
         print('Worker')
     elif choice==3:
+        course_resize.Increase_Seats()
         print('Student')
     else:
         print('Invalid Choice')
