4d75d0657c71bd1471aed21c3b6dee2e1b0b1a6f
saritdi <45334085+saritdi@users.noreply.github.com>
2018-11-25 19:12:41 +0200

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -26,62 +26,6 @@ def InputStudent():
     File_S.write(input('Enter average of the student:'))
     File_S.write('\n')
     
-def Average_Grade():
-    file = open("students.txt",'r')
-    count, total = 0,0
-    for line in file:
-        count+=1
-        total+=int(line.split(' ')[-1])
-    return total/count 
-
-def Average_Grade():
-    course = input("Enter course name: ")
-    if Validate_Course(course):
-        return Calc_Avg(course)
-    
-def Validate_Course(course):
-    file = open("courses.txt",'r')
-    for line in file:
-        if course == line.split(' ')[0]:
-            return True
-    return False
-def Increase_Seats():
-    course = input("Enter course name: ")
-    if Validate_Course(course):
-        size = int(input("Enter new course size: "))
-        return Update_File(course,size)
-    else:
-        print("Invalid Course")
-        return False
-    
-def Validate_Course(course):
-    file = open("courses.txt",'r')
-    for line in file:
-        if course == line.split(' ')[0]:
-            file.close()
-            return True
-    file.close()
-    return False
-
-def Update_File(course,size):
-    file = open("courses.txt",'r')
-    courses = [line for line in file]
-    file.close()
-    for i in range(len(courses)):
-        crs = courses[i].split(" ")
-        if crs[0]==course:
-            if int(crs[1])>size:
-                print("Invalid Size")
-                return False
-            crs[1] = str(size)
-            courses[i]=" ".join(crs)
-            break
-    file = open("courses.txt",'w')
-    for line in courses:
-        file.write(line)
-    file.close()
-    return True
-def Calc_Avg(course):
     
 def M_login():
     print('') 
